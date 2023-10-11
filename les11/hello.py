5d2bfd953ca5a181a2151dd1982a4afeaf9a6958
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2023-10-11 13:17:51 +0000

    fix hello.py

--- les11/hello.py
+++ les11/hello.py
@@ -1,55 +1,49 @@
 #!/usr/bin/env python3
 
-########################################################################################################################
-#
-# (C) 2023 Copyright Aleksei E. Zhuravlev
-#
-# This code is licensed under the Apache License, Version 2.0. You may
-# obtain a copy of this license in the LICENSE-2.0.txt file in the root directory
-# of this source tree or at https://www.apache.org/licenses/LICENSE-2.0
-#
-# Any modifications or derivative works of this code must retain this
-# copyright notice, and modified files need to carry a notice indicating
-# that they have been altered from the originals.
-#
-########################################################################################################################
-#
-# The purpose of this program:
-# 	1. Training (at least mine and whoever wants) of
-# 	the internal structure and principles of the ansible dynamic inventory script
-#	2. Usage in study and work
-#
-########################################################################################################################
-
-import argparse
 import json
+import argparse
+
+def generate_dynamic_inventory():
+    inventory = {
+        'all': {
+            'hosts': [],
+        },
+        '_meta': {
+            'hostvars': {},
+        },
+    }
+
+    hosts = ['hello', 'world']
+    for host in hosts:
+        inventory['all']['hosts'].append(host)
+        inventory['_meta']['hostvars'][host] = {'ansible_host': '127.0.0.1', 'ansible_connection': 'local'}
+
+    return inventory
+
+def get_inventory(args):
+    inventory = generate_dynamic_inventory()
+
+    if args.host:
+        if args.host in inventory['_meta']['hostvars']:
+            host_vars = inventory['_meta']['hostvars'][args.host]
+            inventory = {
+                '_meta': {
+                    'hostvars': host_vars,
+                },
+            }
+        else:
+            inventory = {}
+
+    return inventory
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--list', action="store_true", help='list of all hosts')
-    parser.add_argument('--host', help='only specified HOST')
-#    parser.add_argument('--graph', default action="store_true", help='graph of all hosts or specified group')
+    parser.add_argument('--list', action='store_true', help='Вывести весь список хостов')
+    parser.add_argument('--host', help='Указать конкретный хост')
     args = parser.parse_args()
 
-    hosts = ['hello', 'world']
-
-    inventory = {
-        'all': {
-            'hosts': hosts
-        },
-        '_meta': {
-            'hostvars': {}
-        }
-    }
-
-#    print(f"{inventory=}")
-
-    if args.list:
-        print(json.dumps(inventory))
-    elif args.host:
-        print(hosts[hosts.index(args.host)])
-    else:
-        print("Call error: at least one argument is required!")
+    inventory = get_inventory(args)
+    print(json.dumps(inventory))
 
 if __name__ == '__main__':
     main()
