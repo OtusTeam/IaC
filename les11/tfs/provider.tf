967db00a8d934db3de28ce44087efd1df3feae1f
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2023-10-15 14:52:31 +0000

    tfs added and tested ok

---
+++ les11/tfs/provider.tf
@@ -0,0 +1,14 @@
+provider "yandex" {
+  token     = var.yc_token
+  cloud_id  = var.yc_cloud
+  folder_id = var.yc_folder
+}
+
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
