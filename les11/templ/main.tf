90c21dbf2ab8e504edde52d01630492e2439d620
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2023-10-15 11:55:59 +0000

    templ tf many subnets added and tested ok

--- les11/templ/main.tf
+++ les11/templ/main.tf
@@ -2,14 +2,13 @@ data "yandex_compute_image" "my_image" {
   family = "ubuntu-2204-lts"
 }
 
-
-
 resource "yandex_compute_instance" "my_vm" {
   for_each = local.inventory_data.all.hosts
   name = each.key
 
-  zone        = local.inventory_data.all.vars.sub_zone
+  zone        = local.inventory_data.all.subnets[local.inventory_data.all.hosts[each.key].yandex_cloud_subnet].yandex_cloud_zone
   folder_id   = var.yc_folder
+  platform_id = local.inventory_data.all.hosts[each.key].yandex_cloud_platform
 
   resources {
     cores  = 2
@@ -19,13 +18,11 @@ resource "yandex_compute_instance" "my_vm" {
   boot_disk {
     initialize_params {
       image_id = "${data.yandex_compute_image.my_image.id}"
-#      image_id = data.yandex_compute_image.image_id[each.key].image_id
-#"fd8pecdhv50nec1qf9im"
     }
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.my_sub.id
+    subnet_id = yandex_vpc_subnet.my_sub[local.inventory_data.all.hosts[each.key].yandex_cloud_subnet].id
     nat       = true
   }
 
--- les11/templ/network.tf
+++ les11/templ/network.tf
@@ -3,9 +3,11 @@ resource "yandex_vpc_network" "my_net" {
 }
 
 resource "yandex_vpc_subnet" "my_sub" {
-  name = local.inventory_data.all.vars.sub_name
-  v4_cidr_blocks = [local.inventory_data.all.vars.sub_cidr]
-  zone           = local.inventory_data.all.vars.sub_zone
+  for_each = local.inventory_data.all.subnets
+  name = each.key  
+ 
+  v4_cidr_blocks = [local.inventory_data.all.subnets[each.key].yandex_cloud_cidr]
+  zone           = local.inventory_data.all.subnets[each.key].yandex_cloud_zone
   network_id     = yandex_vpc_network.my_net.id
 }
 
