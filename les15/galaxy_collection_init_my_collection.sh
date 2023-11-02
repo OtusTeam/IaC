ebcff3af70f060eec32f66132ba674174afd5c68
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2023-11-02 09:28:11 +0000

    my.my_collection.plugin.inventory/yacloud_compute fixed and tested ok

--- les15/galaxy_collection_init_my_collection.sh
+++ les15/galaxy_collection_init_my_collection.sh
@@ -2,6 +2,6 @@ cd collections/ansible_collections/
 ansible-galaxy collection init my.my_collection
 cd -
 pwd
-cp -r roles/ansible_authorized_key collections/ansible_collections/my/my_collection/roles/ 
+cp -r my/roles/ansible_authorized_key collections/ansible_collections/my/my_collection/roles/
 mkdir collections/ansible_collections/my/my_collection/plugins/inventory
-cp plugins/inventory/yacloud_compute.py collections/ansible_collections/my/my_collection/plugins/inventory/
+cp my/my_collection/plugins/inventory/yacloud_compute.py collections/ansible_collections/my/my_collection/plugins/inventory/
