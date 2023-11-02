9d6c0a426c4c585bbb5cb38f655a992bd92e57af
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2023-11-02 14:41:48 +0000

    ansible_authorized_key role fixed and tested ok

--- les15/copy_authorized_key_2_ansible_authorized_key.sh
+++
@@ -1,2 +0,0 @@
-cp collections/ansible_collections/devroles/system/roles/authorized_key/tasks/main.yml roles/ansible_authorized_key/tasks/main.yml
-cp collections/ansible_collections/devroles/system/roles/authorized_key/defaults/main.yml roles/ansible_authorized_key/defaults/main.yml
--- les15/galaxy_role_init_ansible_authorized_key.sh
+++ les15/galaxy_role_init_ansible_authorized_key.sh
@@ -1,3 +1,5 @@
 cd roles
 ansible-galaxy role init ansible_authorized_key
-cd ..
+cd -
+cp my/roles/ansible_authorized_key/tasks/main.yml roles/ansible_authorized_key/tasks/
+
