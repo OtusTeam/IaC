10c8e9eb6ea8c8a583b0da999367193791bf6e1b
«Aleksey <Zhuravlev.A.E@gmail.com>
2023-11-27 17:34:17 +0800

    ansible preinstall fixed and tested in one and three ok

---
+++ les23/docker/data/inst_ans.sh
@@ -0,0 +1,5 @@
+sudo apt-get update -y -qq
+sudo apt-get install -y -qq software-properties-common python-software-properties
+sudo add-apt-repository ppa:ansible/ansible -y
+sudo apt-get update -y -qq
+sudo apt-get install -y -qq ansible
