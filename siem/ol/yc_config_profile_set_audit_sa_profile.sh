40d6f8706d876c3c30ca36d04cd445c96524f216
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2024-04-10 08:04:20 +0000

    refactor: sense to sensitive

--- siem/ol/yc_config_profile_set_audit_sa_profile.sh
+++ siem/ol/yc_config_profile_set_audit_sa_profile.sh
@@ -1,5 +1,5 @@
 yc config profile activate audit-sa-profile
-yc config set service-account-key audit_sa_key.json
+yc config set service-account-key audit_sa_key.sensitive.json
 yc config set cloud-id $YC_CLOUD_ID
 yc config set compute-default-zone $YC_ZONE
 yc config set folder-id $YC_GET_FOLDER_ID
---
+++ siem/ol/yc_iam_key_create_audit_sa_key.sh
@@ -0,0 +1,1 @@
+yc iam key create --service-account-name audit-sa --output audit_sa_key.sensitive.json
