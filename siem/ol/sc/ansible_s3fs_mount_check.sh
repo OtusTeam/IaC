da6b67b1f9ffc161ab9792766e90735e52867f47
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2024-04-17 06:18:22 +0000

    sc mount as agent but need fix wazuh

--- siem/ol/sc/ansible_s3fs_mount_check.sh
+++ siem/ol/sc/ansible_s3fs_mount_check.sh
@@ -1,3 +1,3 @@
-ansible -i inv -a 'cat /mount/sc/someprefix/cnpj1fev92kmfcult80h/2024/04/16/054823965.json' sc
+ansible -i inv -a 'sudo -u agent cat /mount/sc/someprefix/cnpj1fev92kmfcult80h/2024/04/16/054823965.json' sc
 
 
--- siem/ol/sc/export.sh
+++ siem/ol/sc/export.sh
@@ -1,5 +1,5 @@
 export SC_ANSIBLE_USER="yc-user"
-export SC_IP="84.201.134.248"
+export SC_IP="158.160.121.151"
 export DEST_IP="158.160.57.166"
 echo sc ansible_host=$SC_IP ansible_user=$SC_ANSIBLE_USER > inv 
 
