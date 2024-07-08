e7552b6fd1440f3afb5a1db726ab263bbcb02c01
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2024-07-08 19:14:24 +0000

    const output fixed and tested ok

--- les03/const/off/output.tf
+++ les03/const/off/output.tf
@@ -1,4 +1,4 @@
 output "public_ip" {
   description = "Public IP address of VM"
-  value = yandex_compute_instance.use_const.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.les03_const.network_interface.0.nat_ip_address
 }
