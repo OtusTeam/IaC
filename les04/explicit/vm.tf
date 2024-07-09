e254c94980803d422ada27dc98dc7847b0d5be08
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2024-07-09 16:56:01 +0000

    explicit added to les04 and tested ok but need use inventory

---
+++ les04/explicit/vm.tf
@@ -0,0 +1,49 @@
+resource "null_resource" "deploy" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      export ANSIBLE_HOST_KEY_CHECKING=False &&
+      ansible -u ${var.username} -i '${yandex_compute_instance.les04_explicit.network_interface.0.nat_ip_address},' --private-key ${var.sec_key_path} -m ping all
+    EOT
+  }
+
+  depends_on = [yandex_compute_instance.les04_explicit]
+}
+
+
+resource "yandex_compute_instance" "les04_explicit" {
+  name = "les04-explicit"
+  zone = var.yc_default_zone
+
+
+  provisioner "remote-exec" {
+    inline = ["sleep 1"]
+
+    connection {
+      host        = self.network_interface.0.nat_ip_address
+      type        = "ssh"
+      user        = var.username
+      private_key = file(var.sec_key_path)
+    }
+  }
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 100
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.yc_image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = var.yc_subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.pub_key_path)}"
+  }
+}
