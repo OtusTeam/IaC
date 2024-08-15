05cc65d93ab02fdab37c594650f8254064581136
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2024-08-15 03:53:01 +0000

    group label added and tested ok

--- Intense2024/les04/bsa/create_vm.sh
+++ Intense2024/les04/bsa/create_vm.sh
@@ -11,12 +11,14 @@ fi
 export VM_NAME="$1"
 export YC_IMAGE_ID="fd8pecdhv50nec1qf9im"
 export YC_SUBNET_ID="e9bop98iu12teftg4uj8"
+export YC_GROUP="webservers"
 
 
 yc compute instance create \
   --name $VM_NAME \
   --hostname $VM_NAME \
   --zone=$YC_ZONE \
+  --labels group=$YC_GROUP \
   --create-boot-disk size=20GB,image-id=$YC_IMAGE_ID \
   --cores=2 \
   --memory=2G \
--- Intense2024/les04/bsa/export_plg.sh
+++
@@ -1,5 +0,0 @@
-set -x
-export ANSIBLE_INVENTORY="../plugin/inventory"
-export ANSIBLE_PLUGINS_INVENTORY="../plugin/plugins/inventory"
-export ANSIBLE_REMOTE_USER="yc-user"
-set +x
