d6369f6e62410812c6a2f520be5f1c05065d28aa
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2024-08-15 14:46:20 +0000

    galaxy added and a little tested

---
+++ Intense2024/les04/galaxy/galaxy_collection_init_my_collection.sh
@@ -0,0 +1,11 @@
+cd collections
+mkdir ansible_collections
+cd ansible_collections
+ansible-galaxy collection init my.my_collection
+cd -
+pwd
+cp -r my/roles/ansible_authorized_key collections/ansible_collections/my/my_collection/roles/
+mkdir collections/ansible_collections/my/my_collection/plugins/inventory
+cp my/my_collection/plugins/inventory/yacloud_compute.py collections/ansible_collections/my/my_collection/plugins/inventory/
+cp my/my_collection/ansible.cfg ./
+cp -r my/my_collection/inventory ./ 
