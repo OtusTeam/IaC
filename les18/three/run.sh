fe36fea071581d666e3c0d29ddd5176629838bff
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2024-09-04 15:07:49 +0000

    les18 added and tested ok

--- les18/three/run.sh
+++ les18/three/run.sh
@@ -4,6 +4,11 @@ read -n 1 -s -r -p "press any key to continue..."; echo ""
 vagrant up
 read -n 1 -s -r -p "press any key to continue..."; echo ""
 curl http://127.0.0.1:8086
+curl http://127.0.0.1:8086
+curl http://127.0.0.1:8086
+curl http://127.0.0.1:8086
+curl http://127.0.0.1:8086
+curl http://127.0.0.1:8086
 read -n 1 -s -r -p "press any key to continue..."; echo ""
 vagrant destroy
 read -n 1 -s -r -p "press any key to continue..."; echo ""
