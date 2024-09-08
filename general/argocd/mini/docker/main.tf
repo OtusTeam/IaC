b6b443db5ec09f4bee75f6c691ac381e50888ef7
Zhuravlev-A-E <Zhuravlev.A.E@gmail.com>
2024-09-08 09:43:41 +0000

    mini and docker installed ok need to test

---
+++ general/argocd/mini/docker/main.tf
@@ -0,0 +1,43 @@
+resource "yandex_compute_instance" "ac_mini_docker" {
+  name = "ac-mini-docker"
+  zone = "ru-central1-a"
+
+  labels = {
+    group = "minidocker"
+  } 
+
+  resources {
+    cores  = 2
+    memory = 8
+    core_fraction = 20
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image # ID образа Ubuntu
+      size = 50
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "${var.username}:${file(var.pub_key_path)}"
+    user-data = <<-EOF
+                #cloud-config
+                package_update: true
+                packages:
+                  - ca-certificates 
+                  - curl 
+                  - gnupg 
+                  - lsb-release
+                EOF
+  }
+}
---
+++ general/argocd/mini/nodrive/ans_inv_list.sh
@@ -0,0 +1,2 @@
+set -x
+ansible-inventory --list
