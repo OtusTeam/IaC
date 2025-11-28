205de804d1ea491349ee8402a32d2105d9bc71f0
«Aleksey <Zhuravlev.A.E@gmail.com>
2025-11-28 14:04:37 +0800

    set and echo added and tested ok

--- les04/explicit/static_website/graph.sh
+++ les04/explicit/static_website/graph.sh
@@ -1,8 +1,6 @@
-terraform validate
+terraform plan
 read -p "Press enter to continue..."
 terraform graph > graph.txt
 cat graph.txt
 read -p "Press enter to continue..."
 terraform graph | dot -Tsvg > graph.svg
-read -p "Press enter to continue..."
-xdg-open graph.svg
