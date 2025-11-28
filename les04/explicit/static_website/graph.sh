8837fc83ed4dd680f042524e2248faf01e84b306
«Aleksey <Zhuravlev.A.E@gmail.com>
2025-11-28 14:09:32 +0800

    use run instead graph in network and graph replaces in  static website

--- les04/explicit/static_website/graph.sh
+++ les04/explicit/static_website/graph.sh
@@ -1,6 +1,11 @@
-terraform plan
+set -x
+terraform validate
 read -p "Press enter to continue..."
 terraform graph > graph.txt
 cat graph.txt
 read -p "Press enter to continue..."
 terraform graph | dot -Tsvg > graph.svg
+read -p "Press enter to continue..."
+xdg-open graph.svg
+set +x
+echo see graph.svg
