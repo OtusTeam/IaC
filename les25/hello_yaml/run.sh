2766cbdd14ab436c135d7797b6f3f03e8c374e67
«Aleksey <Zhuravlev.A.E@gmail.com>
2026-02-09 12:47:27 +0800

    hello world on yaml added and tested ok

---
+++ les25/hello_yaml/run.sh
@@ -0,0 +1,15 @@
+set +x
+# Запрос пасфразы (не отображается при вводе)
+read -rsp "Enter Pulumi passphrase: " PULUMI_CONFIG_PASSPHRASE
+echo
+# Экспортировать только для этого скрипта
+export PULUMI_CONFIG_PASSPHRASE
+pulumi stack init dev
+set -x
+
+pulumi up
+pulumi stack output
+pulumi destroy
+pulumi stack rm dev
+
+set +x
