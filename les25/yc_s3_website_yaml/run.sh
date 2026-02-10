2d5bc6bcd6f33128a41c515f30c4e502f47642b8
«Aleksey <Zhuravlev.A.E@gmail.com>
2026-02-10 18:00:41 +0800

    s3 on yaml added and tested ok

--- les25/yc_s3_website_yaml/run.sh
+++ les25/yc_s3_website_yaml/run.sh
@@ -19,10 +19,15 @@ pulumi config set yandex:token $(yc iam create-token) --secret
 set -x
 pulumi config set yandex:cloudId $YC_CLOUD_ID
 pulumi config set yandex:folderId $YC_FOLDER_ID
-pulumi config set yandex:prefix $YC_PREFIX
+pulumi config set prefix $YC_PREFIX 
 
 pulumi up
 pulumi stack output
+#yc storage object get --bucket "$(pulumi stack output bucketName)" --key "$(pulumi stack output objectKey)" --output hello.txt
+#cat hello.txt
+yc storage bucket get "$(pulumi stack output bucketName)" 
+
+rm hello.txt
 pulumi destroy
 pulumi stack rm dev
 
