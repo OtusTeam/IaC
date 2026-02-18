5af4d6874ba2cfca2c4170611de75b76cae3efdd
«Aleksey <Zhuravlev.A.E@gmail.com>
2026-02-18 14:14:59 +0800

    infra yaml drift refactored and tested ok

--- les25/yc/infra_drift_yaml/run.sh
+++ les25/yc/infra_drift_yaml/run.sh
@@ -22,47 +22,15 @@ pulumi stack output
 
 export SAVED_NAME=$(pulumi stack output subnet_name)
 			    
-read -p "press any key to subnet update to drift on ..."
+read -p "press any key to subnet update and make drift ..."
 yc vpc subnet update \
   --id $(pulumi stack output subnet_id) \
   --folder-id $YC_FOLDER_ID \
   --new-name "$PREFIX-subnet-drifted"  
 
-pulumi refresh --preview-only --diff 
+read -p "press any key to detect and smooth out drift  ..."
 
-read -p "press any key to subnet update to drift off ..."
-yc vpc subnet update \
-  --id $(pulumi stack output subnet_id) \
-  --folder-id $YC_FOLDER_ID \
-  --new-name "$SAVED_NAME" 
-
-pulumi refresh --preview-only --diff
-
-export IP=$(pulumi stack output instance_nat_ip)
-
-sleep 10
-
-while true; do
-  echo "Curl http://$IP/ ..."
-  if curl -fsS --max-time 30 "http://$IP/"; then
-    echo "curl succeeded, exiting."
-    break
-  else
-    echo "curl failed."
-    # спросить пользователя, отменить ли
-    read -rp "cancel (y/N)? " ans
-    case "${ans,,}" in
-      y|yes)
-        echo "Cancelled by user."
-        exit 0
-        ;;
-      *)
-        echo "Retrying..."
-        ;;
-    esac
-  fi
-done
-
+pulumi refresh --diff # --preview-only 
 
 read -p "press any key to destroy and remove stack ..."
 
